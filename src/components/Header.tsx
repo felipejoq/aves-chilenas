8baa23f793a35dbb94cd1c08b04f84d6ada794c9
felipejoq <felipejofreq@gmail.com>
2024-10-01 19:30:59 -0300

    Header componente de astro a componente de react que carga en el cliente

---
+++ src/components/Header.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+interface Props {
+    title?: string;
+}
+
+const Header: React.FC<Props> = ({ title = "Aves misteriosas de Chile" }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <nav className="border border-gray-200 px-2 sm:px-4 py-2.5 bg-gray-800 shadow">
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+                <a href="/" className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            {title}
+          </span>
+                </a>
+
+                <div className="flex items-center">
+                    <button
+                        onClick={toggleMenu}
+                        type="button"
+                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+                    >
+                        <span className="sr-only">Abrir menú</span>
+                        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16m-7 6h7"
+                            />
+                        </svg>
+                    </button>
+                </div>
+
+                <div className={`w-full md:block md:w-auto ${isMenuOpen ? '' : 'hidden'}`} id="mobile-menu">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <a
+                                href="/"
+                                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                                aria-current="page"
+                            >
+                                Home
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
